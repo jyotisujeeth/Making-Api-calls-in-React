fc9c71f0033b26610657f8162eb02c2ba85700c5
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-11 13:38:00 +0530

    added loading concept

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 
 function App() {
 const [movies, setMovies] = useState([]);
+const [isLoading, setIsLoading] = useState(false);
 
 
 async   function fetchMoviesHandler() {
+  setIsLoading = (true);
   const response = await fetch('https://swapi.dev/api/films/');
   const data = await response.json();  
   
@@ -20,7 +22,8 @@ async   function fetchMoviesHandler() {
       };
     });
    setMovies (transformedMovies);
-  
+   setIsLoading( false);
+    
     
 }
 
@@ -31,10 +34,14 @@ async   function fetchMoviesHandler() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} /> }
+        
+        {!isLoading && movies.length > 0 && <p>Found no movies. </p>}
+        {isLoading && <p> Loading...</p>}
       </section>
     </React.Fragment>
   );
+}
 
 
 export default App;
