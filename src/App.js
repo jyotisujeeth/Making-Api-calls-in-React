f3df00dfc403a1c20085edfc1bd1850465c94118
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-13 05:28:37 +0530

    fIRE BASE

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(
-        "https://react--httprequest-default-rtdb.firebaseio.com/movies.json");
+      const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -41,6 +40,7 @@ function App() {
   }, [fetchMoviesHandler]);
 
   function addMovieHandler(movie) {
+    fetch{}
     console.log(movie);
   }
 
